e964e59e5c94fb9a7906bfee4ece4495a1bb3659
Samuel Sampaio <samuelsd@dcc.ufrj.br>
2022-11-24 23:01:21 -0300

    Verification to generator_accountNumber

--- src/Auxiliary.java
+++ src/Auxiliary.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Random;
 
 public abstract class Auxiliary {
 
     static Random myRMgenerator = new Random();
     public static HashMap<String, Account> accountByACNumber = new HashMap<>();
+    public static HashSet<String> acNumberHash = new HashSet<>();
     private static String name;
     private static String CPF;
     private static String ID;
@@ -20,6 +22,7 @@ public abstract class Auxiliary {
     private static String number_account;
     private static double firstValue;
     public static Account account_Creator(String agency){
+        boolean hasAcNumber;
 
         name = InputPersonalData.input_name();
         CPF = InputPersonalData.input_cpf();
@@ -37,19 +40,23 @@ public abstract class Auxiliary {
         Address home = new Address(country, state, city, neighborhood, street, number, complement, CEP);
         Person owner = new Person(name, CPF, ID, home);
 
-        number_account = generator_accountNumber();
+        do{
+            number_account = generator_accountNumber();
+            hasAcNumber = searchAcNumber(number_account);
+        }while (hasAcNumber);
+
+        acNumberHash.add(number_account);
 
         Account account1 = new Account(agency, number_account, owner, firstValue);
-
         accountByACNumber.put(number_account, account1);
 
         return account1;
     }
 
-    //It was used in Main_Test just to test the list
-//    public static void account_CreatorTest(Account ac1){
-//        account_list.add(ac1);
-//    }
+    private static boolean searchAcNumber(String number_account) {
+        return acNumberHash.contains(number_account);
+    }
+
     private static String generator_accountNumber() { //gerar número de conta aleatório
         return String.valueOf(myRMgenerator.nextLong(000000, 999999));
     }
@@ -57,4 +64,6 @@ public abstract class Auxiliary {
     public static Account searchAccount(String acNumber){
         return accountByACNumber.get(acNumber);
     }
+
+
 }
