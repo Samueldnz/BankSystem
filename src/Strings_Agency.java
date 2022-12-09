38c08aab0b3922c0ff0d8b860f0f57c696b5eb8e
Samuel Sampaio <samuelsd@dcc.ufrj.br>
2022-12-09 17:21:31 -0300

    Ideas put away

--- src/Strings_Agency.java
+++ src/Strings_Agency.java
@@ -2,37 +2,31 @@ public abstract class Strings_Agency {
 
     public static void agency_001(){
         System.out.printf("   \n1) Agency: 001\n");
-        System.out.printf("   Avenida Rita Maria Ferreira da Rocha, 500\n");
-        System.out.printf("   Liberdade \n");
+        System.out.printf("   Avenida Rita Maria Ferreira da Rocha, 500, Liberdade \n");
     }
 
     public static void agency_002(){
         System.out.printf("   \n2) Agency: 002\n");
-        System.out.printf("   Avenida Tenente Coronel Adalberto Mendes, 196\n");
-        System.out.printf("   Vila Julieta \n");
+        System.out.printf("   Avenida Tenente Coronel Adalberto Mendes, 196, Vila Julieta \n");
     }
 
     public static void agency_003(){
         System.out.printf("   \n3) Agency: 003\n");
-        System.out.printf("   Rua Prefeito Assumpção, 284\n");
-        System.out.printf("   Vila Julieta \n");
+        System.out.printf("   Rua Prefeito Assumpção, 284, Vila Julieta \n");
     }
 
     public static void agency_004(){
         System.out.printf("   \n4) Agency: 004\n");
-        System.out.printf("   Praça Mariana Rocha Leão, 20\n");
-        System.out.printf("   Vila Odete \n");
+        System.out.printf("   Praça Mariana Rocha Leão, 20, Vila Odete \n");
     }
 
     public static void agency_005(){
         System.out.printf("   \n5) Agency: 005\n");
-        System.out.printf("   Avenida Albino de Almeida, 182\n");
-        System.out.printf("   Comercial \n");
+        System.out.printf("   Avenida Albino de Almeida, 182, Comercial \n");
     }
 
     public static void agency_006(){
         System.out.printf("   \n6) Agency: 006\n");
-        System.out.printf("   Roovia Presidente Dutra, 306\n");
-        System.out.printf("   Jardim do Sol \n");
+        System.out.printf("   Roovia Presidente Dutra, 306, Jardim do Sol \n");
     }
 }
